e0265c83e33cabf5daa798312c22ebb3ade8e8e5
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-17 21:21:14 +0100

    .

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function $(x){
     return document.getElementById(x)
 }
 
+
 window.addEventListener('deviceproximity', function(event) {
     $('proximity').textContent = event.value
-  });
+    // console.log("An object is " + event.value + " centimeters far away");
+    // console.log("Minimum distance sensor can detect is " + event.min + " centimeters");
+    // console.log("Maximum distance sensor can detect is " + event.max + " centimeters");
+});   
+
